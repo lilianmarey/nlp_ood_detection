93141998ff25b2869837bb1cbcf0ac425e0da908
g0bel1n <lucas.saban@icloud.com>
2023-02-22 11:23:56 +0100

    grid_search

--- papier_1/_ood_classifier.py
+++ papier_1/_ood_classifier.py
@@ -2,6 +2,7 @@ import numpy as np
 from aiirw import AI_IRW
 from scipy.spatial.distance import cdist
 from sklearn.base import ClassifierMixin
+from typing import Callable
 
 __available_similarities__ = ["mahalanobis", "IRW"]
 
@@ -14,7 +15,9 @@ def partial_wrapper(f, **kwargs):
 
 
 class OODDetector(ClassifierMixin):
-    def __init__(self, tau: float, similarity: str = "mahalanobis"):
+    def __init__(
+        self, tau: float, base_distribution: np.ndarray, similarity: str = "mahalanobis"
+    ):
         super().__init__()
 
         assert similarity in __available_similarities__, "Similarity is not available"
@@ -22,27 +25,42 @@ class OODDetector(ClassifierMixin):
         self.tau = tau
         self.similarity = similarity
 
-    def fit(self, base_distribution: np.ndarray):
-        # Faire gaffe a pred =0 ou pred =1
         self.base_distribution = base_distribution
+
+        if base_distribution is None:
+            raise ValueError("Base distribution is not provided")
+
+    def clone(self):
+        return self.__class__(
+            tau=self.tau,
+            base_distribution=self.base_distribution,
+            similarity=self.similarity,
+        )
+
+    def _prefit(self):
+        # Faire gaffe a pred =0 ou pred =1
         if self.similarity == "mahalanobis":
-            m = np.mean(base_distribution, axis=0).reshape(
-                1, base_distribution.shape[1]
+            m = np.mean(self.base_distribution, axis=0).reshape(
+                1, self.base_distribution.shape[1]
             )
-            VI = np.linalg.inv(np.cov(base_distribution.T))
+            VI = np.linalg.inv(np.cov(self.base_distribution.T))
             self._compute_sim = partial_wrapper(
                 cdist, XB=m, metric="mahalanobis", VI=VI
             )
         elif self.similarity == "IRW":
             self._compute_sim = lambda x: 1 - partial_wrapper(
                 AI_IRW,
-                X=base_distribution,
+                X=self.base_distribution,
                 n_dirs=1000,
                 AI=False,
                 robust=False,
                 random_state=None,
             )(x)
         return None
+
+    def fit(self, X: np.ndarray, y: np.ndarray):
+        self._prefit()
+        return self
 
     def predict(self, X: np.ndarray) -> np.ndarray:
         if len(X.shape) == 1:
@@ -52,3 +70,15 @@ class OODDetector(ClassifierMixin):
     def score(self, X: np.ndarray, y: np.ndarray) -> float:
         y_pred = self.predict(X)
         return np.mean((y_pred == y).astype(int))
+
+    def get_params(self, deep=True):
+        return {
+            "tau": self.tau,
+            "similarity": self.similarity,
+            "base_distribution": self.base_distribution,
+        }
+
+    def set_params(self, **parameters):
+        for parameter, value in parameters.items():
+            setattr(self, parameter, value)
+        return self
